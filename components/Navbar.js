26822b4ba1de31e359a2fcd9ddeac9fadc84a679
coreyloftus <coreyloftus@gmail.com>
2023-04-01 15:55:57 -0400

    Navbar spacing updated, for 3 separate columns of content

--- components/Navbar.js
+++ components/Navbar.js
@@ -9,7 +9,7 @@ const Navbar = () => {
         <div className="box-border top-0 w-screen bg-gray-800 text-gray-100 p-2 font-poppins z-20">
             <div className="flex items-center justify-between">
                 <Link href="/">
-                    <div className="text-left flex items-center">
+                    <div className="text-left flex items-center hover:animate-pulse">
                         <Image src={logoOnly} alt="Cryolabz Logo" width="50" />
                         <div className="filter brightness-200">
                             <Image src={textOnly} alt="Cryolabz Text" width="150" />
@@ -17,15 +17,20 @@ const Navbar = () => {
                     </div>
                 </Link>
                 {/* Links to other pages */}
-                <Link href="/about">
-                    <button className="border-b-2 border-transparent hover:border-gray-100 p-2">About</button>
-                </Link>
-                <Link href="/contact">
-                    <button className="border-b-2 border-transparent hover:border-gray-100 p-2">Contact</button>
-                </Link>
-                <Link href="/book">
-                    <button className="bg-[#2bbff0] hover:bg-[#2bbff0]/80 rounded-lg p-2">Book a Session</button>
-                </Link>
+                <div>
+                    <p className="font-bold">Free consultations -- Book now!</p>
+                </div>
+                <div>
+                    <Link href="/about">
+                        <button className="mx-4 border-b-2 border-transparent hover:border-gray-100 p-2">About</button>
+                    </Link>
+                    <Link href="/contact">
+                        <button className="mx-4 border-b-2 border-transparent hover:border-gray-100 p-2">Contact</button>
+                    </Link>
+                    <Link href="/book">
+                        <button className="bg-[#2bbff0] hover:bg-[#2bbff0]/80 rounded-lg p-2">Book a Session</button>
+                    </Link>
+                </div>
             </div>
         </div>
     )
