2c10f400ec33c841369136b311f1ae6aa20f1802
coreyloftus <coreyloftus@gmail.com>
2023-04-14 09:55:56 -0400

    Waiver route -> component stub

---
+++ pages/waiver/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const waiver = () => {
+    return (
+        <div className="h-screen">
+            <div className="flex justify-center items-center">
+                <h2 className="text-3xl">Waiver Component</h2>
+            </div>
+        </div>
+    )
+}
+
+export default waiver
